22a63f3071148c5d7e930135c27eb8b9f21bb592
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-12-28 17:51:00 +0100

    chat input finally done

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+"use client"
+import React, { FormEvent } from "react";
+import { useState } from "react"
+
 
 const ChatInput = () => {
+    const [input, setImput] = useState("");
+    const addMessage = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!input) return;
+        const messageToSend = input;
+        setImput("");
+        console.log(input)
+    }
     return (
-        <form className="fixed flex px-10 py-5 w-full bottom-0 z-50 space-x-2 boder-t border-gray-100">
+        <form onSubmit={addMessage} className="fixed flex px-10 py-5 w-full bottom-0 z-50 space-x-2 boder-t border-gray-100">
             <input
+                value={input}
+                onChange={e => setImput(e.target.value)}
                 type="text"
                 placeholder="Enter message here..."
                 className="flex-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-trasparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed "
             />
             <button
                 type="submit"
-                className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disable:opacity-50 disable:cursor-not-allowed"
+                disabled={!input}
+                className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
             >
                 Send
             </button>
