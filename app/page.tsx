34d6f7792714ac24385f601af9b8a247d8fb84ee
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-01-09 15:25:10 +0100

    SSR

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ChatInput from './ChatInput'
 import MessageList from './MessageList'
 
-const HomePage = () => {
+async function HomePage() {
+    const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then((res) => res.json());
     return (
         <main>
             <MessageList />
