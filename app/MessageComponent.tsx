43dd16512d5fd15afbda9e6c5762f3b6656ba77c
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-01-10 15:35:40 +0100

    timeago library

--- app/MessageComponent.tsx
+++ app/MessageComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Message } from '../typings'
 import Image from 'next/image'
 import { useSession } from 'next-auth/react'
+import TimeAgo from "react-timeago"
 
 type Props = {
     message: Message
@@ -24,7 +25,9 @@ export default function MessageComponent({ message }: Props) {
                     <div className={`px-3 py-2 rounded-lg w-fit text-white bg-red-400 ${isUser ? "bg-blue-400 ml-auto order-2" : "bg-red-400"}`}>
                         <p >{message.message}</p>
                     </div>
-                    <p className={`text-[0.65rem] italic px-2 text-gray-400 ${isUser && "text-right"}`}>{new Date(message.created_at).toLocaleString()}</p>
+                    <p className={`text-[0.65rem] italic px-2 text-gray-400 ${isUser && "text-right"}`}>
+                        <TimeAgo date={new Date(message.created_at)} />
+                    </p>
                 </div>
             </div>
         </div>
